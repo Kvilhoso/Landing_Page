2454110dd23ab398c9c983eb9ae4de431fb793e4
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-09-13 18:43:05 -0300

    style: adjust animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,49 +2,52 @@ import { Prism, SplitText } from '@/components';
 
 function App() {
   return (
-    <main className='flex flex-col w-full h-screen bg-zinc-950'>
-      <Prism
-        animationType='rotate'
-        timeScale={0.5}
-        height={3.5}
-        baseWidth={5.5}
-        scale={3.6}
-        hueShift={0}
-        colorFrequency={1}
-        noise={0.5}
-        glow={1}
-      >
-        <div className='flex flex-col w-full h-full items-center justify-center'>
-          <SplitText
-            text='Revolucione suas financias'
-            className='text-4xl font-semibold text-center text-white'
-            delay={100}
-            duration={0.25}
-            ease='power3.out'
-            splitType='chars'
-            from={{ opacity: 0, y: 40 }}
-            to={{ opacity: 1, y: 0 }}
-            threshold={0.1}
-            rootMargin='-100px'
-            textAlign='center'
-          />
+    <main className='flex flex-col w-full h-full bg-zinc-950'>
+      <div className='w-full h-[1080px] relative'>
+        <Prism
+          animationType='rotate'
+          timeScale={0.5}
+          height={4}
+          baseWidth={6}
+          scale={3}
+          hueShift={0}
+          colorFrequency={0.5}
+          noise={0}
+          glow={0.75}
+          hoverStrength={1.5}
+          inertia={0.1}
+        >
+          <div className='flex flex-col w-full h-full items-center justify-center'>
+            <SplitText
+              text='Revolucione suas financias'
+              className='text-6xl font-semibold text-center text-white'
+              delay={70}
+              duration={1}
+              tag='h1'
+              ease='power3.out'
+              splitType='chars'
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              textAlign='center'
+            />
 
-          <SplitText
-            text='Desbloqueando barreiras financeiras'
-            className='text-4xl font-semibold text-center text-white'
-            delay={100}
-            duration={0.25}
-            ease='power3.out'
-            splitType='chars'
-            tag='span'
-            from={{ opacity: 0, y: 40 }}
-            to={{ opacity: 1, y: 0 }}
-            threshold={0.1}
-            rootMargin='-100px'
-            textAlign='center'
-          />
-        </div>
-      </Prism>
+            <SplitText
+              text='Desbloqueando barreiras financeiras'
+              className='text-4xl font-semibold text-center text-white'
+              delay={70}
+              duration={1}
+              tag='h2'
+              ease='power3.out'
+              splitType='chars'
+              from={{ opacity: 0, y: 40 }}
+              to={{ opacity: 1, y: 0 }}
+              threshold={0.1}
+              textAlign='center'
+            />
+          </div>
+        </Prism>
+      </div>
     </main>
   );
 }
