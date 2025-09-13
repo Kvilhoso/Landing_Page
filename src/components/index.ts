22510201709a623dfa0dc3727c8cdcd260e9cd82
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-09-13 20:54:18 -0300

    feat: add `FadeContent` and `ScrollReveal` components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,9 @@
+export * from './FadeContent';
+export * from './Prism';
+export * from './ScrollReveal';
+export * from './SplitText';
+
+export { default as FadeContent } from './FadeContent';
 export { default as Prism } from './Prism';
+export { default as ScrollReveal } from './ScrollReveal';
 export { default as SplitText } from './SplitText';
