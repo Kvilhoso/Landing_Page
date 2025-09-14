c78292d52e521cd5da7bcb119e8bef3d24e1ace1
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-09-14 19:42:43 -0300

    fix: small adjust in name of the project

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,13 @@ const currentYear = new Date().getFullYear();
 export const Footer: FC = () => (
   <footer className='flex w-full max-w-7xl h-min justify-between items-center mt-10 px-4 pt-10 bg-zinc-900 border-t border-t-zinc-800'>
     <p className='text-zinc-100 text-lg font-montreal-light'>
-      © {currentYear} projekt Rage. Todos os direitos reservados.
+      © {currentYear} projeKt Rage. Todos os direitos reservados.
     </p>
 
     <img
       src={LOGO_CIRCLE}
-      alt='projekt Rage'
-      title='projekt Rage'
+      alt='projeKt Rage'
+      title='projeKt Rage'
       className='size-7 rounded-full object-contain'
     />
   </footer>
