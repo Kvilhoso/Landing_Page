73656e2ab7eb18413d1c241abba2d26afe5b3347
Daniel Sansão Araldi <danielsaraldi@gmail.com>
2025-09-14 14:25:53 -0300

    feat: add image logo in `Footer` component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,19 @@
 import type { FC } from 'react';
+import { LOGO_CIRCLE } from '@/config/images';
 
 const currentYear = new Date().getFullYear();
 
 export const Footer: FC = () => (
-  <footer className='flex w-full max-w-7xl h-min justify-start items-center mt-10 pt-10 bg-zinc-900 border-t border-t-zinc-800'>
+  <footer className='flex w-full max-w-7xl h-min justify-between items-center mt-10 px-4 pt-10 bg-zinc-900 border-t border-t-zinc-800'>
     <p className='text-zinc-100 text-lg font-montreal-light'>
       © {currentYear} projekt Rage. Todos os direitos reservados.
     </p>
+
+    <img
+      src={LOGO_CIRCLE}
+      alt='projekt Rage'
+      title='projekt Rage'
+      className='size-7 rounded-full object-contain'
+    />
   </footer>
 );
